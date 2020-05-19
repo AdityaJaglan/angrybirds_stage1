4efd31ba048be0cf1e9942736ffe12a4fa523e1d
AdityaJaglan <64016924+AdityaJaglan@users.noreply.github.com>
2020-05-19 16:49:03 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,19 +1,6 @@
-class Box{
-constructor(x,y,width,height){
-    var options={
-        "restitution":1,
-        "friction":0.3,
-        "densty":1.0
-    }
-    this.body=Bodies.rectangle(x,y,width,height,options);
-    this.width=width
-    this.height=height
-    World.add(world,this.body);
-}
-display(){
-    var pos=this.body.position;
-    rectMode(CENTER);
-    fill(255);
-    rect(pos.x,pos.y,this.width,this.height);
+class Box extends BaseClass{
+  constructor(x, y, width, height) {
+   super(x, y,width,height)
+   this.image=loadImage("sprites/wood1.png")
 }
 }
